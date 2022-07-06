fd97585dd6295c670df2a8a986283f18b3fc8f43
Munenori Saito <Saito_Munenori@ogis-ri.co.jp>
2022-07-06 09:49:58 +0900

    create id by sequence

--- nautible-app-ms-stock-azure/src/main/java/jp/co/ogis_ri/nautible/app/stock/outbound/cosmosdb/CosmosdbStockAllocateHistoryRepositoryImpl.java
+++ nautible-app-ms-stock-azure/src/main/java/jp/co/ogis_ri/nautible/app/stock/outbound/cosmosdb/CosmosdbStockAllocateHistoryRepositoryImpl.java
@@ -5,8 +5,13 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.bson.Document;
+
 import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepositoryBase;
 import io.quarkus.panache.common.Parameters;
@@ -21,6 +26,8 @@ import jp.co.ogis_ri.nautible.app.stock.exception.DuplicateRequestException;
 @ApplicationScoped
 public class CosmosdbStockAllocateHistoryRepositoryImpl
         implements StockAllocateHistoryRepository, PanacheMongoRepositoryBase<CosmosdbStockAllocateHistory, Integer> {
+    /** 在庫引当履歴テーブル名 */
+    private static final String STOCK_ALLOCATE_HISTORY_TABLE_NAME = "StockAllocateHistory";
     @Inject
     MongoClient mongoClient;
     @Inject
@@ -28,10 +35,14 @@ public class CosmosdbStockAllocateHistoryRepositoryImpl
 
     @Override
     public StockAllocateHistory create(StockAllocateHistory stockAllocateHistory) {
+        CosmosdbStockAllocateHistory cosmosdbStockAllocateHistory= cosmosdbStockAllocateHistoryMapper
+                    .stockAllocateHistoryToCosmosdbStockAllocateHistory(stockAllocateHistory);
+
+        int sequence = getSequenceNumber(STOCK_ALLOCATE_HISTORY_TABLE_NAME);
+        cosmosdbStockAllocateHistory.setId(sequence);
 
         try {
-            persist(cosmosdbStockAllocateHistoryMapper
-                    .stockAllocateHistoryToCosmosdbStockAllocateHistory(stockAllocateHistory));
+            persist(cosmosdbStockAllocateHistory);
         } catch (MongoWriteException e) {
             if (e.getCode() == 11000) {
                 //E11000 duplicate key error collection:
@@ -74,4 +85,19 @@ public class CosmosdbStockAllocateHistoryRepositoryImpl
                 .cosmosdbStockAllocateHistoryToStockAllocateHistory(find("RequestId", requestId).list());
     }
 
+       /**
+     * Mongodbのfunctionでシーケンスを発番する。
+     * @param tableName テーブル名
+     * @return シーケンス
+     */
+    private int getSequenceNumber(String tableName) {
+        // 本来はマイクロサービスの管理単位を跨ぐような（データベースを跨ぐような）DBアクセスは禁止。
+        // サービス毎にシーケンスCollectionを作成するとCosmosdbのコストが高くなる、また作業簡略化のためCommonDBへのアクセスを行う。
+        // 時間ができたら共通サービスを作成して発番機能を作る。
+        Document result = mongoClient.getDatabase("Common").getCollection("Sequence").findOneAndUpdate(
+                Filters.eq("_id", tableName),
+                new Document("$inc", new Document("SequenceNumber", 1)),
+                new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
+        return result.getInteger("SequenceNumber");
+    }
 }
